28c1bdcbb18fe3afac9f5701b0f777ad649e9c9c
Yu Zhao <zyu0321@gmail.com>
2017-10-03 01:38:03 -0400

    use async

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const async = require("async");
 
 //declare aws sdk
 const AWS = require("aws-sdk");
 
-// // declare axios for making http requests
-// const axios = require("axios");
-// const API = "https://jsonplaceholder.typicode.com";
-
 //config AWS
-AWS.config.loadFromPath("./credential.json");
+AWS.config.credentials = {
+  accessKeyId: "AKIAIJ4PTT2SZP7XYOBQ",
+  secretAccessKey: "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",
+  region: "us-east-1"
+};
 
 // Create S3 service object
 s3 = new AWS.S3({ apiVersion: "2006-03-01" });
@@ -34,24 +35,39 @@ router.get("/buckets", (req, res) => {
 
 // List all Objects of a bucket, create and respond with the url list.
 router.get("/objects", (req, res) => {
-  let params = { Bucket: "yzhao583-new-test-bucket" };
-  let urlList = [];
+  var params = { Bucket: "yzhao583-new-test-bucket" };
+  var urlList = [];
   s3.listObjects(params, (err, data) => {
-    if(err) {
+    if (err) {
       res.status(500).send(err);
     } else {
-      let bucketContents = data.Contents;
-      for (let i = 0; i < bucketContents.length; i++) {
-        let urlParams = {
-          Bucket: "yzhao583-new-test-bucket",
-          Key: bucketContents[i].Key
-        };
-        s3.getSignedUrl("getObject", urlParams, function(err, url){
-          urlList.push({ 'url': url });
-        });
-      }
-      console.log(urlList);
-      res.status(200).send(urlList);
+      var bucketContents = data.Contents;
+      async.each(
+        bucketContents,
+        (content, done) => {
+          var urlParams = {
+            Bucket: "yzhao583-new-test-bucket",
+            Key: content.Key
+          };
+          s3.getSignedUrl("getObject", urlParams, (err, url) => {
+            if (err) {
+              res.status(500).send(err);
+              return;
+            } else {
+              urlList.push({ 'url': url });
+              done();
+            }
+          });
+        },
+        err => {
+          if (err) {
+            res.status(500).send(err);
+            return;
+          } else {
+            res.status(200).send(urlList);
+          }
+        }
+      );
     }
   });
 });
